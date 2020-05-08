facf878fede371d367a7b2fec09963f140d9d0e7
Tom Robinson <thomas.robinson@noaa.gov>
2020-05-08 12:30:17 -0500

    Improves ESM4_run.sh to set stacksize correctly

--- run/ESM4_run.sh
+++ run/ESM4_run.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
+ ￼
+#***********************************************************************
+#                   GNU Lesser General Public License
+#
+# This file is part of the GFDL Flexible Modeling System (FMS).
+#
+# FMS is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# FMS is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+#***********************************************************************
 
 ###########################################################
 ##  ############## USER INPUT SECTION ################## ##
 ###########################################################
-#directory where the model will run
+## directory where the model will run
 ## This directory should contain the input.nml file, *_table, INPUT, and RESTART folders.
-workDir=/path/to/workDir
+workDir=PATH2workDir
 #Path to executable
-executable=/path/to/esm4.1.x
+executable=${PWD}../exec/esm4.1.x
 #MPI run program (srun, mpirun, mpiexec, aprun ...)
 run_prog=srun
 #Option to specify number of cores
@@ -21,6 +40,8 @@ atm_cores=1728
 atm_threads=2
 #Number of cores to run the ocean
 ocn_cores=1437
+## Add any additional options here that you need for your system
+
 ###########################################################
 ##  ############# END USER INPUT SECTION ############### ##
 ###########################################################
@@ -29,10 +50,13 @@ export KMP_STACKSIZE=512m
 export NC_BLKSZ=1M
 export F_UFMTENDIAN=big
 
-#To open files with newer HDF5
-export HDF5_USE_FILE_LOCKING=FALSE
+## Set the stacksize to unlimited
+ulimit unlimited
+ulimit -S -s unlimited
+ulimit -S -c unlimited
 
 ## Go to the workDir
-cd ${workdir}
+cd ${workDir}
 
+## Execute the model in the workDir
 ${run_prog} ${ncores}  ${atm_cores} ${nthreads} ${atm_threads} ${executable} : ${ncores} ${ocn_cores} ${executable} |& tee stdout.log
