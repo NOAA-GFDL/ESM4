9521a9c5acc4bb395dd1392d8dddecd209ef99ab
Tom Robinson <thomas.robinson@noaa.gov>
2021-01-14 10:13:54 -0500

    Include -fbacktrace in gnu template for fortran

--- exec/templates/gnu.mk
+++ exec/templates/gnu.mk
@@ -82,7 +82,7 @@ FPPFLAGS := $(INCLUDES)
 FPPFLAGS += $(shell nf-config --fflags)
 
 # Base set of Fortran compiler flags
-FFLAGS := -fcray-pointer -fdefault-real-8 -fdefault-double-8 -Waliasing -ffree-line-length-none -fno-range-check -fallow-invalid-boz -fallow-argument-mismatch
+FFLAGS := -fcray-pointer -fdefault-real-8 -fdefault-double-8 -Waliasing -ffree-line-length-none -fno-range-check -fallow-invalid-boz -fallow-argument-mismatch -fbacktrace
 
 # Flags based on perforance target (production (OPT), reproduction (REPRO), or debug (DEBUG)
 FFLAGS_OPT = -O2 -fno-expensive-optimizations
